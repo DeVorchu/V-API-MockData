87343d22d24952773893f91a2baca38713edc010
Vorczu <vorczu89@gmail.com>
2022-08-17 07:37:19 +0200

    Add - module export on users data

---
+++ db/users.js
@@ -0,0 +1,9 @@
+const users =[
+    {
+        id: 1,
+        nick: 'Vorczu',
+        pw:'test'
+    }
+]
+
+module.exports = users;
--- index.js
+++ index.js
@@ -3,28 +3,22 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express();
 
+const users = require('./db/users')
+
 require('dotenv').config();
 
-const users =[
-    {
-        id: 1,
-        nick: 'Vorczu',
-        pw:'test'
-    }
-]
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 router.post("/login",(req,res) => {
     const loginRes = users.find(x => x.nick === req.body.nick )
-    console.log(loginRes);
 
     if (typeof loginRes !== "undefined") {
-        console.log("here");
+        //TO DO: Auth on 200 
         loginRes.pw === req.body.pw ? res.status(200).send('OK') : res.status(401).send('Bad password!')
     } else {
         res.status(404).send('User not found!')
-        console.log("here3");
     }
 });
     
